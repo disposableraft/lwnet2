75b6b61fa8d13fd044dc50930de36105261a47b3
Lance W <50839441+disposableraft@users.noreply.github.com>
2022-05-06 18:13:35 -0700

    Rebuild site when files change

--- src/server/main.go
+++ src/server/main.go
@@ -4,15 +4,54 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/fsnotify/fsnotify"
+	"lancewakeling.net/website/src/gatsby"
 )
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./docs"))
 	http.Handle("/", fileServer)
 
-	fmt.Println("Starting server at port http://localhost:8080")
+	// ---
 
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				gatsby.BuildSite()
+
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("templates")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Starting server at port http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+
+	<-done
 }
